aded35d82f905e135cd6276aa8e8982fd37f5ff0
Supreet0 <150177964+Supreetpatel@users.noreply.github.com>
2024-12-14 17:17:04 +0530

    Ninenth

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,11 +8,11 @@ const Footer = () => {
             <div>
                 <p className="font-semibold text-gray text-xs">
                     More ways to shop:  {' '}
-                    <span className="underline text-blue">
+                    <span className="underline text-blue cursor-pointer">
                     Find an Apple Store {' '}
                     </span>
                     or {' '}
-                    <span className="underline text-blue">
+                    <span className="underline text-blue cursor-pointer">
                     Other retailer {' '}
                     </span> {' '}
                     near you.
